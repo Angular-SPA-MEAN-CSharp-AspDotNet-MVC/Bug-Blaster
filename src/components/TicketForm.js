6811f2a30de6a1c7e8b0f6eae056343021d1b289
Kun Huang <knhg2013@gmail.com>
2025-04-06 11:25:05 -0400

    Sec 8, Lec 69 tik list done so far

---
+++ src/components/TicketForm.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+export default function TicketForm({ dispatch }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState(`1`);
+
+  const prioirityLabels = {
+    1: "Low",
+    2: "Medium",
+    3: "Hight",
+  };
+
+  const clearForm = () => {
+    setTitle("");
+    setDescription("");
+    setPriority("1");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const ticketData = {
+      id: new Date().toISOString(),
+      title,
+      description,
+      priority,
+    };
+
+    dispatch({
+      type: "ADD_TICKET",
+      payload: ticketData,
+    });
+
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='ticket-form'>
+      <div>
+        <label>Title</label>
+        <input
+          type='text'
+          value={title}
+          className='form-input'
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          type='text'
+          value={description}
+          className='form-input'
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <fieldset className='priority-fieldset'>
+        <legend>Priority</legend>
+        {Object.entries(prioirityLabels).map(([value, label]) => (
+          <label key={value} className='priority-label'>
+            <input
+              type='radio'
+              className='priority-input'
+              value={value}
+              checked={priority === value}
+              onChange={(e) => setPriority(e.target.value)}
+            ></input>
+            {label}
+          </label>
+        ))}
+      </fieldset>
+      <button type='submit' className='button'>
+        Submit
+      </button>
+    </form>
+  );
+}
